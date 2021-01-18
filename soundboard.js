6d372e18f71e9dbaf1b23d15c4db950549ef1d78
alim127 <audreylim127@gmail.com>
2021-01-18 01:56:01 -0500

    sound

---
+++ soundboard.js
@@ -0,0 +1,85 @@
+var sound1 = document.getElementById("audio1");
+var sound2 = document.getElementById("audio2");
+var sound3 = document.getElementById("audio3");
+var sound4 = document.getElementById("audio4");
+var sound5 = document.getElementById("audio5");
+var sound6 = document.getElementById("audio6");
+var sound7 = document.getElementById("audio7");
+var sound8 = document.getElementById("audio8");
+var sound9 = document.getElementById("audio9");
+var sound10 = document.getElementById("audio10");
+var sound11 = document.getElementById("audio11");
+var sound12 = document.getElementById("audio12");
+
+function play_sound_1()
+{
+    sound1.play();
+
+}
+
+function play_sound_2()
+{
+    
+    sound2.play();
+
+}
+
+function play_sound_3()
+{
+    sound3.play();
+
+}
+
+function play_sound_4()
+{
+    sound4.play();
+
+}
+
+function play_sound_5()
+{
+    sound5.play();
+
+}
+
+function play_sound_6()
+{
+    sound6.play();
+
+}
+
+function play_sound_7()
+{
+    sound7.play();
+
+}
+
+function play_sound_8()
+{
+    sound8.play();
+
+}
+
+function play_sound_9()
+{
+    sound9.play()
+
+}
+
+function play_sound_10()
+{
+    sound10.play();
+
+}
+
+function play_sound_11()
+{
+    sound11.play();
+
+}
+
+function play_sound_12()
+{
+    sound12.play();
+
+}
